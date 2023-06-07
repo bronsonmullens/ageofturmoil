dcf3dbe4c07f8549084ba90770a43b97bd7170cd
Bronson <bronsonmullens@icloud.com>
2023-06-07 14:17:43 -0700

    Initial Commit

---
+++ Age of Turmoil/Age_of_TurmoilApp.swift
@@ -0,0 +1,17 @@
+//
+//  Age_of_TurmoilApp.swift
+//  Age of Turmoil
+//
+//  Created by Bronson Mullens on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Age_of_TurmoilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Age of Turmoil/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Age of Turmoil
+//
+//  Created by Bronson Mullens on 6/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
